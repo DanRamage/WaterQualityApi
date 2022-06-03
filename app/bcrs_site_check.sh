a39d3d1c5ee7bc92c7229123d3dec04b97fc77a2
DANRAMAGE <ChiefDan@gmail.com>
2022-06-03 11:41:02 -0400

    Added script to periodically check for new BCRS sites at various locations.

--- app/bcrs_site_check.sh
+++ app/bcrs_site_check.sh
@@ -8,6 +8,6 @@ export FLASK_APP=/var/www/devapihowsthebeach/app/manage.py
 
 flask get_bcrs_sites --params follybeach "32.569375 -80.043630,32.750204 -79.807029"
 
-flask get_bcrs_sites --params charleston "32.569375 -80.043630,32.750204 -79.807029"
+flask get_bcrs_sites --params charleston "32.569375 -80.043630,32.860402 -79.685711"
 
 flask get_bcrs_sites --params sarasota "27.547242 -82.524490,27.255850 -82.814941"
