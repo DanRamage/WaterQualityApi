bed7c2691c41f82b7a6853a995eccee826453908
DANRAMAGE <ChiefDan@gmail.com>
2022-06-03 11:15:56 -0400

    Added

--- app/bcrs_site_check.sh
+++ app/bcrs_site_check.sh
@@ -7,3 +7,7 @@ cd /var/www/devapihowsthebeach/app
 export FLASK_APP=/var/www/devapihowsthebeach/app/manage.py
 
 flask get_bcrs_sites --params follybeach "32.569375 -80.043630,32.750204 -79.807029"
+
+flask get_bcrs_sites --params charleston "32.569375 -80.043630,32.750204 -79.807029"
+
+flask get_bcrs_sites --params sarasota "27.547242 -82.524490,27.255850 -82.814941"
