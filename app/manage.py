fe109a29ab36b5ea3132d44c3bdaab58c8506315
DANRAMAGE <ChiefDan@gmail.com>
2024-04-04 11:16:38 -0400

    Reworked the import sample sites management function.

--- app/manage.py
+++ app/manage.py
@@ -5,7 +5,6 @@ export FLASK_APP=<fullpathto>/manage.py
 """
 import sys
 sys.path.append('../commonfiles/python')
-import os
 import click
 from flask import Flask, current_app, redirect, url_for, request
 import logging.config
@@ -22,6 +21,7 @@ import requests
 import shapely
 from shapely.wkb import loads as wkb_loads
 from shapely.geometry import Point, Polygon, box
+import pandas as pd
 
 #from sqlalchemy import MetaData
 #from sqlalchemy import create_engine
@@ -134,63 +134,94 @@ def build_boundaries(params):
       current_app.logger.exception(e)
 
   current_app.logger.debug("build_boundaries finished in %f seconds" % (time.time()-start_time))
+'''
+import_sample_sites 
+  --params <location> <sample sites csv> <sites column json file> <boundaries csv>
+Given the project name, the sample site csv, sites json file mapping CSV columns to DB, and boundary csv, this populates the sample_site and boudnary tables.
 
-#import_sample_sites --params sarasota /Users/danramage/Documents/workspace/WaterQuality/Florida_Water_Quality/config/sample_sites_boundary.csv /Users/danramage/Documents/workspace/WaterQuality/Florida_Water_Quality/config/sarasota_boundaries.csv
-#Given the project name, the sample site csv and boundary csv, this populates the sample_site and boudnary tables.
-@app.cli.command()
-@click.option('--params', nargs=3)
+GilsCreek:
+{\"name\": \"Id\", \"description\": \"Name\", \"county\": \"County\", \"longitude\": \"Long\", \"latitude\": \"Lat\"} 
+'''
+@app.cli.command('import_sample_sites')
+@click.option('--params', nargs=5)
 def import_sample_sites(params):
   start_time = time.time()
   init_logging(app)
   area_name = params[0]
   sample_site_csv = params[1]
-  boundaries_file = params[2]
+  sample_site_column_json_file = json.loads(params[2])
+  site_type = params[3]
+  boundaries_file = params[4]
   current_app.logger.debug("import_sample_sites started.")
 
-  wq_sites = wq_sample_sites()
-  wq_sites.load_sites(file_name=sample_site_csv, boundary_file=boundaries_file)
+  sites_df = pd.read_csv(sample_site_csv)
 
   row_entry_date = datetime.now()
-
-  area_rec = db.session.query(Project_Area)\
-    .filter(Project_Area.area_name == area_name).first()
+  try:
+    area_rec = db.session.query(Project_Area)\
+      .filter(Project_Area.area_name == area_name).first()
+    if area_rec is None:
+      current_app.logger.error(f"Area: {area_name} has not been defined.")
+      return -1
+  except Exception as e:
+    e
+  try:
+    site_type_rec = db.session.query(Site_Type)\
+    .filter(Site_Type.name == site_type)\
+    .one()
+  except Exception as e:
+    current_app.logger.exception(e)
   #ADd the boundaries first
-  for site in wq_sites:
-    for contained_by in site.contained_by:
-      try:
-        bound = Boundary()
-        bound.row_entry_date = row_entry_date.strftime('%Y-%m-%d %H:%M:%S')
-        bound.project_site_id = area_rec.id
-        bound.boundary_name = contained_by.name
-        bound.wkb_boundary = contained_by.object_geometry.wkb
-        bound.wkt_boundary = contained_by.object_geometry.to_wkt()
-        current_app.logger.debug("Adding boundary: %s" % (bound.boundary_name))
-        db.session.add(bound)
-        db.session.commit()
-      except Exception as e:
-        current_app.logger.exception(e)
-        db.session.rollback()
-
-  for site in wq_sites:
+  '''
+  if len(boundaries_file): 
+    for site in wq_sites:
+      for contained_by in site.contained_by:
+        try:
+          bound = Boundary()
+          bound.row_entry_date = row_entry_date.strftime('%Y-%m-%d %H:%M:%S')
+          bound.project_site_id = area_rec.id
+          bound.boundary_name = contained_by.name
+          bound.wkb_boundary = contained_by.object_geometry.wkb
+          bound.wkt_boundary = contained_by.object_geometry.to_wkt()
+          current_app.logger.debug("Adding boundary: %s" % (bound.boundary_name))
+          db.session.add(bound)
+          db.session.commit()
+        except Exception as e:
+          current_app.logger.exception(e)
+          db.session.rollback()
+  '''
+  for ndx, input_row in sites_df.iterrows():
     try:
       site_rec = Sample_Site()
-      site_rec.project_site_id = area_rec.id
       site_rec.row_entry_date = row_entry_date.strftime('%Y-%m-%d %H:%M:%S')
-      site_rec.site_name = site.name
-      site_rec.description = site.description
-      site_rec.county = site.county
-      site_rec.longitude = site.object_geometry.x
-      site_rec.latitude = site.object_geometry.y
-      site_rec.wkt_location = site.object_geometry.wkt
+      site_rec.project_site_id = area_rec.id
+      site_rec.site_type_id = site_type_rec.id
+
+      site_name = input_row[sample_site_column_json_file['name']]
+      site_rec.site_name = site_name
+      description = input_row[sample_site_column_json_file['description']]
+      site_rec.description = description
+      county = input_row[sample_site_column_json_file['county']]
+      site_rec.county = county
+      longitude = input_row[sample_site_column_json_file['longitude']]
+      site_rec.longitude = longitude
+      latitude = input_row[sample_site_column_json_file['latitude']]
+      site_rec.latitude = latitude
+      wkt_location = f"POINT({site_rec.longitude} {site_rec.latitude})"
+      site_rec.wkt_location = wkt_location
+
       site_rec.temporary_site = False
       #Look up boundaries
+      '''
       for contained_by in site.contained_by:
         boundary_rec = db.session.query(Boundary)\
           .filter(Boundary.boundary_name == contained_by.name).first()
         site_rec.boundaries.append(boundary_rec)
+      '''
       current_app.logger.debug("Adding site: %s" % (site_rec.site_name))
       db.session.add(site_rec)
       db.session.commit()
+
     except Exception as e:
       current_app.logger.exception(e)
       db.session.rollback()
